70b3bc10c6c8bb4ae3f6934f11aa211e5bf67f34
kbFACTS <44099433+kbFACTS@users.noreply.github.com>
2024-07-04 15:37:37 -0400

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,25 +1,25 @@
 import React from "react";
 
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Link,
-  } from "react-router-dom";
+// import {
+//     BrowserRouter as Router,
+//     Routes,
+//     Route,
+//     Link,
+//   } from "react-router-dom";
 
 import Linkbox from "./linkbox";
 
-import {
-    AiFillCustomerService,
-    AiFillHome
-  } from "react-icons/ai";
-import { blue, grey } from "@mui/material/colors";
+// import {
+//     AiFillCustomerService,
+//     AiFillHome
+//   } from "react-icons/ai";
+// import { blue, grey } from "@mui/material/colors";
 
 const Home = () => {
     return (
         <div className="home">
             <div className="content-1 roundcontainer shadow">
-                <img src="nokeif-logo.svg" className="home-img"></img>
+                <img alt="" src="nokeif-logo.svg" className="home-img"></img>
                 <div className="pdb">
                     <Linkbox id="spotify" title="Spotify" url="https://open.spotify.com/artist/3AoiNh85pj8qfchXmgeh43?si=ziPF19-ZT2mLHHpfU2t-Pw" />
                     <Linkbox id="tiktok" title="TikTok" url=""/>
