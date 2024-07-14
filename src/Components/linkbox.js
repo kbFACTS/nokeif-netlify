cbda88de0645755a4a3bf9a333917d7dc7ef2289
Keith B <kjbasara2014@gmail.com>
2024-07-14 18:26:46 -0400

    Alignment of link titles

--- src/Components/linkbox.js
+++ src/Components/linkbox.js
@@ -9,7 +9,7 @@ const Linkbox = (props) => {
             <div id={props.id} className="linkbox shadow">
                 <a className="linkbox-link" href={props.url} rel="noreferrer" target="_blank">
                     <img alt="" src={props.icon} className="icon"></img>
-                    <span>{props.title}</span>
+                    <div className="link-title">{props.title}</div>
                 </a>
             </div>
         </div>
